c862bdc65bfb6bbcb3017b169b96fd640758388c
Masrianto <45374438+masriant@users.noreply.github.com>
2025-01-18 15:04:45 +0700

    Update custom-functions.php
    Shortcode untuk Menampilkan Tabel Post dengan Filter Kategori dari Pengaturan Admin:
    
    Shortcode: [admin_filtered_posts_table]
    Deskripsi: Menampilkan tabel yang berisi daftar post dengan kategori yang difilter berdasarkan pengaturan di admin (kategori yang dikecualikan dapat disimpan dalam pengaturan situs).
    
    Shortcode untuk Menampilkan Tabel Post dengan Filter Kategori Statis:
    
    Shortcode: [static_filtered_posts_table]
    Deskripsi: Menampilkan tabel yang berisi daftar post dengan kategori yang difilter berdasarkan daftar kategori statis yang ditentukan di dalam kode.

--- mu-plugins/custom-functions.php
+++ mu-plugins/custom-functions.php
@@ -4,16 +4,96 @@
  * This file contains all custom shortcodes and filters.
  */
 
-// Shortcode 1: Display posts in a table
-function shortcode_display_posts_table() {
-    // Code for displaying posts in a table
-}
-add_shortcode('posts_table', 'shortcode_display_posts_table');
+// Shortcode 1: Display posts in a table filtered by admin settings
+function display_admin_filtered_posts_table() {
+    $excluded_categories = get_option('tdp_excluded_categories', array());
 
-// Shortcode 2: Filter categories
-function shortcode_filter_categories() {
-    // Code for filtering categories
-}
-add_shortcode('filter_categories', 'shortcode_filter_categories');
+    $args = array(
+        'post_type' => 'post',
+        'post_status' => 'publish',
+        'posts_per_page' => -1,
+    );
 
-// Additional filters and functions
+    $query = new WP_Query($args);
+    $output = '<table border="1" cellpadding="10" cellspacing="0" style="width: 100%; margin-bottom: 20px;">';
+    $output .= '<thead><tr><th>Judul Post</th><th>Kategori</th></tr></thead>';
+    $output .= '<tbody>';
+
+    while ($query->have_posts()) {
+        $query->the_post();
+        $post_title = get_the_title();
+        $post_link = get_permalink();
+
+        $categories = get_the_category();
+        $category_links = array();
+
+        foreach ($categories as $category) {
+            if (!in_array($category->slug, $excluded_categories)) {
+                $category_links[] = '<a href="' . get_category_link($category->term_id) . '">' . $category->name . '</a>';
+            }
+        }
+
+        if (!empty($category_links)) {
+            $categories_output = implode(', ', $category_links);
+            $output .= '<tr>';
+            $output .= '<td><a href="' . $post_link . '">' . $post_title . '</a></td>';
+            $output .= '<td>' . $categories_output . '</td>';
+            $output .= '</tr>';
+        }
+    }
+
+    wp_reset_postdata();
+    $output .= '</tbody>';
+    $output .= '</table>';
+
+    return $output;
+}
+add_shortcode('admin_filtered_posts_table', 'display_admin_filtered_posts_table');
+
+// Shortcode 2: Display posts in a table filtered by static category list
+function display_static_filtered_posts_table() {
+    $excluded_categories = array('uncategorized', 'other-category-slug');
+
+    $args = array(
+        'post_type' => 'post',
+        'post_status' => 'publish',
+        'posts_per_page' => -1,
+    );
+
+    $query = new WP_Query($args);
+    $output = '<table border="1" cellpadding="10" cellspacing="0" style="width: 100%; margin-bottom: 20px;">';
+    $output .= '<thead><tr><th>Judul Post</th><th>Kategori</th></tr></thead>';
+    $output .= '<tbody>';
+
+    while ($query->have_posts()) {
+        $query->the_post();
+        $post_title = get_the_title();
+        $post_link = get_permalink();
+
+        $categories = get_the_category();
+        $category_links = array();
+
+        foreach ($categories as $category) {
+            if (!in_array($category->slug, $excluded_categories)) {
+                $category_links[] = '<a href="' . get_category_link($category->term_id) . '">' . $category->name . '</a>';
+            }
+        }
+
+        if (!empty($category_links)) {
+            $categories_output = implode(', ', $category_links);
+            $output .= '<tr>';
+            $output .= '<td><a href="' . $post_link . '">' . $post_title . '</a></td>';
+            $output .= '<td>' . $categories_output . '</td>';
+            $output .= '</tr>';
+        }
+    }
+
+    wp_reset_postdata();
+    $output .= '</tbody>';
+    $output .= '</table>';
+
+    return $output;
+}
+add_shortcode('static_filtered_posts_table', 'display_static_filtered_posts_table');
+
+?>
